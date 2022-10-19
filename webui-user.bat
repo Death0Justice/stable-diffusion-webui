975998855763f709abe0a8ad476749d453141079
Death0Justice <Death_Justice@163.com>
2022-10-19 15:44:39 +0100

    Add vscode custom settings to .gitignore
    Add element tag template and --no-half-vae

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\Python\Python310\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--xformers --no-half-vae --opt-channelslast
 
 call webui.bat
